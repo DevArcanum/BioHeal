1fd4e4724c0afcb02ecc7c45553a208d9dab7f82
Vaseem Ahmed <vaseem@arcanum.com>
2024-11-03 19:05:10 +0530

    React App

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const App = () => {
+   return <div style={{color:"black"}}>
+    <p>Heyeyeye</p>Hello, World!</div>;
+};
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,67 @@
 const path = require("path");
 const webpack = require("webpack");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 
 module.exports = {
-  entry: "./index.js",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve("dist"),
-    publicPath: "/",
-  },
-  module: {
-    rules:[
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: "babel-loader"
-      },
-      {
-        test: /\.html$/,
-        use: "html-loader"
-      },
-      /*Choose only one of the following two: if you're using 
-      plain CSS, use the first one, and if you're using a
-      preprocessor, in this case SASS, use the second one*/
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.scss$/,
-        use:[
-          "style-loader",
-          "css-loader",
-          "sass-loader"
+    entry: "./index.js",
+    output: {
+        filename: "bioheal.bundle.js",
+        path: path.resolve("dist"),
+        publicPath: "/",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            ['@babel/preset-react', { targets: "defaults" }]
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.html$/,
+                use: "html-loader"
+            },
+            /*Choose only one of the following two: if you're using 
+            plain CSS, use the first one, and if you're using a
+            preprocessor, in this case SASS, use the second one*/
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ],
+            },
+            {
+                test: /\.jpe?g$|\.png$|\.svg$|\.wav$/,
+                type:'asset/resource'
+            },
+            {
+                test: /\.ico$/,
+                type: 'asset/resource',
+                generator:{
+                    filename:'[name][ext]'
+                }
+            }
         ],
-      },
-    ], 
-  },  
-  plugins: [
-    new HTMLWebpackPlugin({
-      template: "index.html"
-    }),
-  ]
+    },
+    plugins: [
+        new webpack.ProgressPlugin(),
+        new CleanWebpackPlugin(),
+        new HTMLWebpackPlugin({
+            template: "index.html"
+        }),
+    ],
+    stats: 'errors-only',
 }
